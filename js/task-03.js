7fa051bc3b1ad3c2c463e9eeb3f15e4ab8c11b0c
Anna Shevchenko <uaskoa@gmail.com>
2020-11-28 23:32:10 +0200

    HW done

--- js/task-03.js
+++ js/task-03.js
@@ -1,25 +1,6 @@
-// const ADMIN_PASSWORD = "jqueryismyjam";
-// let message;
-// const userInput = prompt("Введите пароль");
-
-// console.log(userInput);
-
-// if (userInput === null) {
-//   console.log((message = "Отменено пользователем!"));
-// } else if (userInput === ADMIN_PASSWORD) {
-//   console.log((message = "Добро пожаловать!"));
-// } else if (userInput !== ADMIN_PASSWORD) {
-//   console.log((message = "Доступ запрещен, неверный пароль!"));
-// }
-
-// alert(message);
-
-
 const ADMIN_PASSWORD = "jqueryismyjam";
 let message;
 const userInput = prompt("Введите пароль");
-
-// console.log(userInput);
 
 if (userInput === null) {
   message = "Отменено пользователем!";
@@ -30,5 +11,3 @@ if (userInput === null) {
 }
 
 alert(message);
-
-
--- js/task-05.js
+++ js/task-05.js
@@ -3,9 +3,6 @@ let message;
 const userInput = prompt("Введите страну доставки");
 const normalizedInput = userInput.toLowerCase();
 
-// const normilizedСountry =
-//   userСountry.charAt(0).toUpperCase() + userСountry.slice(1);
-
 console.log(normalizedInput);
 
 switch (normalizedInput) {
@@ -34,50 +31,8 @@ switch (normalizedInput) {
     message = `Доставка в ${normalizedInput} будет стоить ${cost} кредитов.`;
     break;
 
-    default:
+  default:
     message = "В вашей стране доставка не доступна";
 }
 
- alert (message)
-
-// // Пользователь может оформить доставку товара к себе в страну, указав ее при
-// // посещении страницы в prompt.Учти, пользователь может ввести имя страны не только
-// // буквами нижнего регистра, а к примеру 'кИтАЙ'.
-
-// // Напиши скрипт который выводит сообщение о стоимости доставки в указанную страну.
-// // Обязательно используй switch.Формат сообщения: 'Доставка в [страна] будет стоить
-// // цена] кредитов'.
-
-// // Но доставка есть не везде, если указанной страны нет в списке, то выводи в
-// // alert сообщение 'В вашей стране доставка не доступна'.
-
-// // Ниже приведен список стран и стоимость доставки.
-
-// // Китай - 100 кредитов
-// // Чили - 250 кредитов
-// // Австралия - 170 кредитов
-// // Индия - 80 кредитов
-// // Ямайка - 120 кредитов
-
-
-// let cost;
-// const subscription = 'premium';
-
-// switch (subscription) {
-//   case 'free':
-//     cost = 0;
-//     break;
-
-//   case 'pro':
-//     cost = 100;
-//     break;
-
-//   case 'premium':
-//     cost = 500;
-//     break;
-
-//   default:
-//     console.log('Invalid subscription type');
-// }
-
-// console.log(cost); // 500
+alert(message);
