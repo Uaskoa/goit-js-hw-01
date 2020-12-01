b0ffd54779ff85d39717722047e40d237395a8fd
Anna Shevchenko <uaskoa@gmail.com>
2020-12-02 00:11:05 +0200

    update-task-05

--- js/task-05.js
+++ js/task-05.js
@@ -1,38 +1,44 @@
 let cost;
 let message;
-const userInput = prompt("Введите страну доставки");
-const normalizedInput = userInput.toLowerCase();
+let userInput = prompt("Введите страну доставки");
 
-console.log(normalizedInput);
 
-switch (normalizedInput) {
-  case "китай":
-    cost = 100;
-    message = `Доставка в ${normalizedInput} будет стоить ${cost} кредитов.`;
-    break;
+if (userInput !== null) {
+  userInput = userInput.toLowerCase;
 
-  case "чили":
-    cost = 250;
-    message = `Доставка в ${normalizedInput} будет стоить ${cost} кредитов.`;
-    break;
+  switch (userInput) {
+    case "китай":
+      cost = 100;
+      message = `Доставка в ${normalizedInput} будет стоить ${cost} кредитов.`;
+      break;
 
-  case "австралия":
-    cost = 170;
-    message = `Доставка в ${normalizedInput} будет стоить ${cost} кредитов.`;
-    break;
+    case "чили":
+      cost = 250;
+      message = `Доставка в ${normalizedInput} будет стоить ${cost} кредитов.`;
+      break;
 
-  case "индия":
-    cost = 80;
-    message = `Доставка в ${normalizedInput} будет стоить ${cost} кредитов.`;
-    break;
+    case "австралия":
+      cost = 170;
+      message = `Доставка в ${normalizedInput} будет стоить ${cost} кредитов.`;
+      break;
 
-  case "ямайка":
-    cost = 120;
-    message = `Доставка в ${normalizedInput} будет стоить ${cost} кредитов.`;
-    break;
+    case "индия":
+      cost = 80;
+      message = `Доставка в ${normalizedInput} будет стоить ${cost} кредитов.`;
+      break;
 
-  default:
-    message = "В вашей стране доставка не доступна";
+    case "ямайка":
+      cost = 120;
+      message = `Доставка в ${normalizedInput} будет стоить ${cost} кредитов.`;
+      break;
+
+    default:
+      message = "В вашей стране доставка не доступна";
+  }
+
+  alert(message);
+
+} else {
+
+  alert("Вы отменили доставку");
 }
-
-alert(message);
